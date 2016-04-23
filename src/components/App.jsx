2c6cad22251db2fbab933f6ebb98334160bf60f5
EQuimper <quimperemanuel@gmail.com>
2016-04-23 13:57:00 -0600

    Initial Commit

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const App = React.createClass({
+  render() {
+    return (
+      <div className="col-lg-offset-4 col-lg-4">
+        <h1>HELLO</h1>
+	      <h1>Style me with <a href="http://flexboxgrid.com/" target="_blank">FlexboxGrid</a></h1>
+      </div>
+    );
+  }
+});
+
+export default App;
